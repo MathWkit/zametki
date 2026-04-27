783ba4bad15d178c0167e95ad17e5cc0a7cceae3
Mathew <mathwkit@gmail.com>
2026-04-27 20:38:57 +0300

    добавил метод BlockTypeCodec::fromString

--- src/storage/json/block_type_codec.cpp
+++ src/storage/json/block_type_codec.cpp
@@ -16,5 +16,25 @@ QString BlockTypeCodec::toString(const core::BlockType type)
 
     return QStringLiteral("paragraph");
 }
+
+core::BlockType BlockTypeCodec::fromString(const QString &value)
+{
+    if (value == QStringLiteral("paragraph"))
+    {
+        return core::BlockType::Paragraph;
+    }
+
+    if (value == QStringLiteral("heading"))
+    {
+        return core::BlockType::Heading;
+    }
+
+    if (value == QStringLiteral("todo"))
+    {
+        return core::BlockType::Todo;
+    }
+
+    return core::BlockType::Paragraph;
+}
 }
 
