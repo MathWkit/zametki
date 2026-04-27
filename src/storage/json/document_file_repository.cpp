b0c3840ae29e117d4c01ecea2b37e02d526ac95d
Mathew <mathwkit@gmail.com>
2026-04-27 21:31:06 +0300

    реализовал read() в file_repository

--- src/storage/json/document_file_repository.cpp
+++ src/storage/json/document_file_repository.cpp
@@ -1,7 +1,12 @@
 #include "storage/json/document_file_repository.h"
 
+#include <QFile>
+#include <QJsonDocument>
 #include <QDir>
 #include <QDebug>
+
+#include "storage/json/document_json_serializer.h"
+
 
 namespace zametki::storage::json
 {
@@ -12,8 +17,35 @@ DocumentFileRepository::DocumentFileRepository(const QString &notesPath)
 
 core::Document DocumentFileRepository::read(const QString &id) const
 {
-    Q_UNUSED(id)
-    return {};
+    const QString filePath = documentFilePath(id);
+    QFile file(filePath);
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+    {
+        qWarning().noquote() << QStringLiteral("Failed to open document file for reading:") << filePath;
+        return {};
+    }
+
+    const QByteArray data = file.readAll();
+    file.close();
+
+    QJsonParseError parseError;
+    const QJsonDocument doc = QJsonDocument::fromJson(data, &parseError);
+
+    if (parseError.error != QJsonParseError::NoError)
+    {
+        qWarning().noquote() << QStringLiteral("JSON parse error:") << parseError.errorString();
+        return {};
+    }
+
+    if (!doc.isObject())
+    {
+        qWarning() << QStringLiteral("JSON document is not an object");
+        return {};
+    }
+
+    DocumentJsonSerializer serializer;
+    return serializer.deserialize(doc.object());
 }
 
 bool DocumentFileRepository::write(const QString &id, const core::Document &document) const
