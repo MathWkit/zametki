ab0c26f2dc8964503b445a226c80da3d1427b3c3
Mathew <mathwkit@gmail.com>
2026-04-28 23:40:07 +0300

    изменил id_generator

--- include/core/id_generator.h
+++ include/core/id_generator.h
@@ -10,12 +10,22 @@ class IdGenerator
 public:
     virtual ~IdGenerator() = default;
     virtual QString create() const = 0;
+
+    virtual quint32 createSiteId() const = 0;
+
+    virtual QString createOperationId() const = 0;
 };
 
 class UuidIdGenerator final : public IdGenerator
 {
 public:
     QString create() const override;
+    quint32 createSiteId() const override;
+    QString createOperationId() const override;
+
+private:
+    static quint32 getSiteId();
+    static quint32 getAndIncrementOperationCounter();
 };
 }
 
