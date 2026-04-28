ed3e90541e2735ddfabbd09e03140c27e8e0fb42
Mathew <mathwkit@gmail.com>
2026-04-28 23:51:40 +0300

    исправил document_json_serializer

--- include/storage/json/document_json_serializer.h
+++ include/storage/json/document_json_serializer.h
@@ -2,6 +2,7 @@
 #define ZAMETKI_DOCUMENT_JSON_SERIALIZER_H
 
 #include <QJsonObject>
+#include <QString>
 
 #include "core/blocks/heading_block.h"
 #include "core/blocks/paragraph_block.h"
@@ -10,23 +11,29 @@
 
 namespace zametki::storage::json
 {
+
 class DocumentJsonSerializer
 {
 public:
     QJsonObject serialize(const core::Document &document) const;
     core::Document deserialize(const QJsonObject &object) const;
 
+    static QString getCurrentFormatVersion();
+    static bool supportsVersion(const QString &version);
+    static bool validateFormatVersion(const QJsonObject &object, QString &error);
+
 private:
-    QJsonObject serializeParagraphBlock(const core::ParagraphBlock &block) const;
-    QJsonObject serializeHeadingBlock(const core::HeadingBlock &block) const;
-    QJsonObject serializeTodoBlock(const core::TodoBlock &block) const;
-    QJsonObject serializeBlock(const core::Block &block) const;
+    static QJsonObject serializeParagraphBlock(const core::ParagraphBlock &block);
+    static QJsonObject serializeHeadingBlock(const core::HeadingBlock &block);
+    static QJsonObject serializeTodoBlock(const core::TodoBlock &block);
+    static QJsonObject serializeBlock(const core::Block &block);
     core::Block deserializeBlock(const QJsonObject &object) const;
     bool validateDocumentObject(const QJsonObject &object, QString &error) const;
-    bool validateBlockObject(const QJsonObject &object, QString &error) const;
+    static bool validateBlockObject(const QJsonObject &object, QString &error);
     core::ParagraphBlock deserializeParagraphBlock(const QJsonObject &object) const;
     core::HeadingBlock deserializeHeadingBlock(const QJsonObject &object) const;
     core::TodoBlock deserializeTodoBlock(const QJsonObject &object) const;
+    static core::Document migrateFromV1ToV2(const core::Document &docV1);
 };
 }
 
